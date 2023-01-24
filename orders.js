25af117c3404d2556a04c4a3b5fb3d448df6f996
Nkosinathi Kelvin <nkosikev_n@pop-os.localdomain>
2023-01-24 08:49:01 +0200

    first commit

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Micro Foldable Drone',
+        productNumber: '28513',
+        paymentStatus:'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Samurai K1545 Drone',
+        productNumber: '30206',
+        paymentStatus:'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Razor AR2K Drone',
+        productNumber: '27395',
+        paymentStatus:'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Revolution-3 Camera Drone',
+        productNumber: '21690',
+        paymentStatus:'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Stealth-tracker AK1800 Drone',
+        productNumber: '01831',
+        paymentStatus:'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Grim-Reaper 32 Series-X Drone',
+        productNumber:'21324',
+        paymentStatus:'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Sasuke 17 Speed Drone',
+        productNumber: '06577',
+        paymentStatus:'Delivered',
+        shipping: 'Delivered'
+    }
+]
